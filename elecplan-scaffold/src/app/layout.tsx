157a73619eca9cf4ae60e267d0e9048fefc5d9b5
ElecplanElectrical <lukedphillips@hotmail.com>
2026-08-18 10:40:08 +1000

    Use Elecplan mark for Home Screen icon

--- elecplan-scaffold/src/app/layout.tsx
+++ elecplan-scaffold/src/app/layout.tsx
@@ -8,8 +8,8 @@ export const metadata: Metadata = {
   applicationName: "Elecplan Team",
   manifest: "/manifest.webmanifest",
   icons: {
-    icon: "/elecplan-app-icon.svg",
-    apple: "/elecplan-app-icon.svg",
+    icon: "/app-icon.png",
+    apple: "/app-icon.png",
   },
   appleWebApp: {
     capable: true,
