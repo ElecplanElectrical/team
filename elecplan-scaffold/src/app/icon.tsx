40cca0a50da0484a837bfda56be8a70a5550a2f1
ElecplanElectrical <lukedphillips@hotmail.com>
2026-08-18 15:44:41 +1000

    Zoom new square Elecplan logo for app icon

---
+++ elecplan-scaffold/src/app/icon.tsx
@@ -0,0 +1,39 @@
+import { ImageResponse } from "next/og";
+
+export const size = {
+  width: 512,
+  height: 512,
+};
+
+export const contentType = "image/png";
+
+const SOURCE = "https://team.elecplan.com.au/5EAC5C26-D2E9-4219-8FB7-FDD38093BAFE.png?v=11";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "relative",
+          overflow: "hidden",
+          background: "#000000",
+        }}
+      >
+        <img
+          src={SOURCE}
+          alt=""
+          style={{
+            position: "absolute",
+            left: "-80px",
+            top: "-80px",
+            width: "672px",
+            height: "672px",
+          }}
+        />
+      </div>
+    ),
+    size,
+  );
+}
