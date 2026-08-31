cfa406a07f6ada1f31fa8f54b37c5af64b6d2d3c
ElecplanElectrical <lukedphillips@hotmail.com>
2026-08-30 18:17:03 -0700

    Use approved resources page design directly

--- elecplan-scaffold/src/app/resources/page.tsx
+++ elecplan-scaffold/src/app/resources/page.tsx
@@ -1,3 +1,2 @@
 import { InfoPage } from "@/components/yourplan-public";
-import { LockedPublicFrame } from "@/components/locked-public-frame";
-export default function Page(){return <LockedPublicFrame active="Resources"><InfoPage kind="resources"/></LockedPublicFrame>}
+export default function Page(){return <InfoPage kind="resources"/>}
