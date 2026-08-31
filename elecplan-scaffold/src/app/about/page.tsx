f98888236aba7acab6c641f664a7da9ebce93961
ElecplanElectrical <lukedphillips@hotmail.com>
2026-08-30 18:16:53 -0700

    Use approved about page design directly

--- elecplan-scaffold/src/app/about/page.tsx
+++ elecplan-scaffold/src/app/about/page.tsx
@@ -1,3 +1,2 @@
 import { InfoPage } from "@/components/yourplan-public";
-import { LockedPublicFrame } from "@/components/locked-public-frame";
-export default function Page(){return <LockedPublicFrame active="About"><InfoPage kind="about"/></LockedPublicFrame>}
+export default function Page(){return <InfoPage kind="about"/>}
