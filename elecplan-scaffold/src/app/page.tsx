24767711121c85f05f24690ea3a36b92687f41c0
ElecplanElectrical <lukedphillips@hotmail.com>
2026-09-08 01:45:50 -0700

    Redirect installed YourPlan app root to login

--- elecplan-scaffold/src/app/page.tsx
+++ elecplan-scaffold/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { LockedExactHome } from "@/components/locked-exact-home";
 import { LockedMobileHome } from "@/components/locked-mobile-home";
+import { StandaloneLoginRedirect } from "@/components/standalone-login-redirect";
 
 export default function RootPage() {
   return (
     <>
+      <StandaloneLoginRedirect />
       <div className="hidden md:block"><LockedExactHome /></div>
       <div className="md:hidden"><LockedMobileHome /></div>
     </>
